d4443173624c991ef4072bf2e7ea3d2e6f1492cc
kingurru <kingnikoriginal@gmail.com>
2021-07-01 16:07:30 +0300

    repair search func

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -3,15 +3,15 @@ import {connect} from "react-redux";
 import {NavLink} from "react-router-dom";
 import './Users.css'
 import {deleteId, setAllowedUsersId, setCurrentUser, setLoading, setUsers} from "../../redux/actions/auth";
-import axios_ from "../../axios/axios";
 import {clearLocalTasks} from "../../redux/actions/tasks";
 import Input from "../../components/UI/Input/Input";
 
-function Users({setLoading, state, setAllowedUsersId, setCurrentUser, setUsers, clearLocalTasks}) {
+function Users({setLoading, state, setAllowedUsersId, setCurrentUser, clearLocalTasks}) {
 
   const admin = JSON.parse(localStorage.getItem('currentPerson'))
   const persons = JSON.parse(localStorage.getItem('personList'))
-
+  const [name, setName] = useState('')
+  const [nickname, setNickname] = useState('')
   const [filteredUsers, setFilteredUsers] = useState(null)
 
   const usersOfThisAdmin = Object.values(persons?.users).filter(user => user.yourAdmin === admin.email)
@@ -23,24 +23,19 @@ function Users({setLoading, state, setAllowedUsersId, setCurrentUser, setUsers, 
     setCurrentUser(user)
   }
 
-  const filterUsers = (val, arr, method) => {
-    console.log(val, arr)
-    if (method === 'name') {
-      return arr.filter(user => user.name.toLowerCase().includes(val) || user.surname.toLowerCase().includes(val))
-    } else if (method === 'nickname') {
-      return arr.filter(user => user.nickname.includes(val))
-
-    }
+  const filterUsers = () => {
+    const users = usersOfThisAdmin.filter(user => user.name.toLowerCase().includes(name) || user.surname.toLowerCase().includes(name)).filter(user => user.nickname.includes(nickname))
+    setFilteredUsers(users)
   }
 
   const filterByName = e => {
     const val = e.target.value.toLowerCase()
-    setFilteredUsers(filterUsers(val, usersOfThisAdmin, 'name'))
+    setName(val)
   }
 
   const filterByNickname = e => {
     const val = e.target.value.toLowerCase()
-    setFilteredUsers(filterUsers(val, usersOfThisAdmin, 'nickname'))
+    setNickname(val)
   }
 
 
@@ -54,17 +49,9 @@ function Users({setLoading, state, setAllowedUsersId, setCurrentUser, setUsers, 
       </NavLink>
     </li>)
 
-  const fetchUsers = async () => {
-    setLoading()
-    try {
-      await axios_.get('/todo.json')
-        .then((res) => {
-          setUsers(res.data.users)
-        })
-    } catch (e) {
-      console.log(e)
-    }
-  }
+  useEffect(() => {
+    filterUsers()
+  }, [name, nickname])
 
   useEffect(() => {
     setAllowedUsersId(listUserId)
